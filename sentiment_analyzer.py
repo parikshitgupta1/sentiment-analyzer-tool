ab530a4104b30c895672abe9fd239098f3f1e353
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2019-10-08 02:46:34 +0530

    Update sentiment_analyzer.py

--- sentiment_analyzer.py
+++ sentiment_analyzer.py
@@ -1,4 +1,4 @@
-import streamlit as s
+import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import confusion_matrix
